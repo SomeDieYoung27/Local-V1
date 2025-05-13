75ba478a1d4873d1d91da1fa55edb315d223b42e
Shashwat Chaturvedi <111681395+SomeDieYoung27@users.noreply.github.com>
2025-05-13 22:37:01 +0530

    Feat :Session history code

---
+++ localbase/chain.py
@@ -0,0 +1,15 @@
+import re
+from operator import itemgetter
+from typing import List
+
+from langchain.schema.runnable import RunnablePassthrough
+from langchain_core.documents import Document
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import Runnable
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.tracers.stdout import ConsoleCallbackHandler
+from langchain_core.vectorstores import VectorStoreRetriever
+
+from localbase.config import Config
+from localbase.session_history import SessionHistory
