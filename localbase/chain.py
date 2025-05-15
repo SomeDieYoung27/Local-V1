a0fea900a7d0ce9218e4c0d2ccf57a2f891d419a
Shashwat Chaturvedi <111681395+SomeDieYoung27@users.noreply.github.com>
2025-05-15 14:10:39 +0530

    Feat :Uploader code added

--- localbase/chain.py
+++ localbase/chain.py
@@ -13,3 +13,17 @@ from langchain_core.vectorstores import VectorStoreRetriever
 
 from localbase.config import Config
 from localbase.session_history import SessionHistory
+
+SYSTEM_PROMPT = """
+Utilize the provided contextual information to respond to the user question.
+If the answer is not found within the context, state that the answer cannot be found.
+Prioritize concise responses (maximum of 3 sentences) and use a list where applicable.
+The contextual information is organized with the most relevant source appearing first.
+Each source is separated by a horizontal rule (---).
+
+Context:
+{context}
+
+Use markdown formatting where appropriate.
+"""
+
